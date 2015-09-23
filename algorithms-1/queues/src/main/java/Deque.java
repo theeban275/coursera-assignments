89638cf8639c35a45b8f0157a1eb597099e48247
Theeban Soundararajan <theeban@intersect.org.au>
2015-09-23 20:41:52 +1000

    add methods addFirst and addLast

--- algorithms-1/queues/src/main/java/Deque.java
+++ algorithms-1/queues/src/main/java/Deque.java
@@ -1,9 +1,72 @@
-public class Deque {
+import java.util.Iterator;
 
-    public Deque() {
+public class Deque<Item> implements Iterable<Item> {
+
+    private class Node {
+        public Item item;
+        public Node next;
     }
 
-    public boolean isEmpty() {
-        return true;
+    private Node first;
+    private Node last;
+    private int size;
+
+    public Deque() {
+        this.size = 0;
+    }
+
+    public void addFirst(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+
+        if (first == null) {
+            first = new Node();
+            first.item = item;
+            last = first;
+        } else {
+            Node oldFirst = first;
+            first = new Node();
+            first.item = item;
+            first.next = oldFirst;
+        }
+
+        size++;
+    }
+
+    public void addLast(Item item) {
+        if (item == null) {
+             throw new NullPointerException();
+        }
+
+        if (last == null) {
+            last = new Node();
+            last.item = item;
+            first = last;
+        } else {
+            Node oldLast = last;
+            last = new Node();
+            last.item = item;
+            if (oldLast != null) {
+                oldLast.next = last;
+            }
+        }
+
+        size++;
+    }
+
+    public Item[] toArray() {
+        @SuppressWarnings("unchecked")
+        Item[] items = (Item[]) new Object[size];
+        Node n = first;
+        for (int i = 0; i < items.length; i++) {
+            items[i] = n.item;
+            n = n.next;
+        }
+        return items;
+    }
+
+    public Iterator<Item> iterator() {
+        return null;
     }
 }
--- algorithms-1/queues/src/test/java/DequeTest.java
+++ algorithms-1/queues/src/test/java/DequeTest.java
@@ -1,19 +1,68 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class DequeTest {
 
-    private Deque deque;
+    private Deque<String> deque;
 
     @Before
     public void setup() {
-       deque = new Deque();
+       deque = new Deque<String>();
     }
 
     @Test
-    public void testIsEmptyReturnsTrue() {
-        assertEquals(deque.isEmpty(), true);
+    public void testAddFirstOnEmptyQueue() {
+        deque.addFirst("one");
+        assertTrue(Arrays.equals(deque.toArray(), new String[]{ "one" }));
     }
+
+    @Test
+    public void testAddFirstAddsItemsToFrontOfQueue() {
+        deque.addFirst("one");
+        deque.addFirst("two");
+        assertTrue(Arrays.equals(deque.toArray(), new String[]{ "two", "one" }));
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void testAddFirstRaisesExceptionForNullItem() {
+        deque.addFirst(null);
+    }
+
+    @Test
+    public void testAddLastOnEmptyQueue() {
+         deque.addLast("one");
+         assertTrue(Arrays.equals(deque.toArray(), new String[] { "one" }));
+    }
+
+    @Test
+    public void testAddLastAddsItemsToEndOfQueue() {
+        deque.addLast("one");
+        deque.addLast("two");
+        assertTrue(Arrays.equals(deque.toArray(), new String[] { "one", "two" }));
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void testAddLastRaisesExceptionForNullItem() {
+        deque.addLast(null);
+    }
+
+    @Test
+    public void testAddLastAfterAddFirst() {
+         deque.addFirst("one");
+         deque.addLast("two");
+         assertTrue(Arrays.equals(deque.toArray(), new String[] { "one", "two" }));
+    }
+
+    @Test
+    public void testAddFirstAfterAddLast() {
+         deque.addLast("one");
+         deque.addFirst("two");
+         assertTrue(Arrays.equals(deque.toArray(), new String[] { "two", "one" }));
+    }
+
 }
