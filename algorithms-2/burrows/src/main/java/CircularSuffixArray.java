22bb63b4871978e882cd41521b7a7df3c647a188
Theeban Soundararajan <theeban@intersect.org.au>
2016-05-07 17:50:26 -0700

    add CircularSuffixArray implementation

--- algorithms-2/burrows/src/main/java/CircularSuffixArray.java
+++ algorithms-2/burrows/src/main/java/CircularSuffixArray.java
@@ -1,15 +1,91 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
 public class CircularSuffixArray {
 
-    public CircularSuffixArray(String s) {
+    private static class CircularString implements CharSequence, Comparable<CircularString> {
 
+        private final CharSequence sequence;
+        private final int offset;
+
+        public CircularString(CharSequence s, int offset) {
+            this.sequence = s;
+            this.offset = offset;
+        }
+
+        public int offset() {
+            return offset;
+        }
+
+        @Override
+        public int length() {
+            return sequence.length();
+        }
+
+        @Override
+        public char charAt(int index) {
+            return sequence.charAt(index + offset);
+        }
+
+        @Override
+        public CharSequence subSequence(int start, int end) {
+            return new CircularString(sequence.subSequence(start, end), offset);
+        }
+
+        @Override
+        public IntStream chars() {
+            return sequence.chars();
+        }
+
+        @Override
+        public IntStream codePoints() {
+            return sequence.codePoints();
+        }
+
+        @Override
+        public int compareTo(CircularString o) {
+            for (int i = 0; i < length() && i < o.length(); i++) {
+                if (charAt(i) < o.charAt(i)) {
+                    return -1;
+                } else if (charAt(i) > o.charAt(i)) {
+                    return 1;
+                }
+            }
+            return length() - o.length();
+        }
+    }
+
+    private final CircularString[] suffixes;
+
+    public CircularSuffixArray(String s) {
+        if (s == null) {
+            throw new NullPointerException("Argument is null");
+        }
+
+        suffixes = new CircularString[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            suffixes[i] = new CircularString(s, i);
+        }
+
+        Arrays.sort(suffixes, new Comparator<CircularString>() {
+            @Override
+            public int compare(CircularString o1, CircularString o2) {
+                return o1.compareTo(o2);
+            }
+        });
     }
 
     public int length() {
-        return -1;
+        return suffixes.length;
     }
 
     public int index(int i) {
-        return -1;
+        if (i < 0 || i >= length()) {
+           throw new IndexOutOfBoundsException("index " + i + " is out of bounds");
+        }
+
+        return suffixes[i].offset();
     }
 
 }
--- algorithms-2/burrows/src/test/java/CircularSuffixArrayTest.java
+++ algorithms-2/burrows/src/test/java/CircularSuffixArrayTest.java
@@ -1,2 +1,44 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CircularSuffixArrayTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testNullString() {
+        suffixArray(null);
+    }
+
+    @Test
+    public void testLengthEmptyString() {
+        assertEquals(0, suffixArray("").length());
+    }
+
+    @Test
+    public void testLengthString() {
+        assertEquals(12, suffixArray("ABRACADABRA!").length());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexNegative() {
+        suffixArray("ABRACADABRA!").index(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexOutOfBounds() {
+        suffixArray("ABRACADABRA!").index(13);
+    }
+
+    @Test
+    public void testIndex() {
+        CircularSuffixArray suffix = suffixArray("ABRACADABRA!");
+        assertEquals(11, suffix.index(0));
+        assertEquals(0, suffix.index(3));
+        assertEquals(6, suffix.index(9));
+    }
+
+    private CircularSuffixArray suffixArray(String s) {
+        return new CircularSuffixArray(s);
+    }
+
 }
